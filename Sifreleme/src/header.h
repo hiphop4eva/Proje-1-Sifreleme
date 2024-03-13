d147853bd1e4cb320f241717a78c2be2ee8fd7ab
hiphop4eva <burak_-_yavuz@hotmail.com>
2024-03-13 21:48:58 +0300

    Added win32 UI

---
+++ Sifreleme/src/header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <Windows.h>
+#include <ShObjIdl.h>
--- Sifreleme/src/src.cpp
+++ Sifreleme/src/src.cpp
@@ -1,8 +1,97 @@
-#include <iostream>
+#include "header.h"
 
-int main() {
-	std::cout << "Hello World!" << std::endl;
-	std::cin.get();
+HWND inputString, outputString;
+HWND inputMove;
+HWND checkCaesar, checkEncode, checkDecode, checkFile;
+HWND buttonGo;
+
+HRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wparam, LPARAM lparam);
+
+int WINAPI wWinMain(HINSTANCE hinstance, HINSTANCE hpreviousinstance, PWSTR pCmdLine, int nCmdShow) {
+	
+	//PWSTR windowName = L"Main Window";
+
+	WNDCLASS windowMain = {};
+	windowMain.hInstance = hinstance;
+	windowMain.lpszClassName = L"Main Window";
+	windowMain.lpfnWndProc = WindowProc;
+
+	RegisterClass(&windowMain);
+
+	HWND hwnd = CreateWindowEx(0, L"Main Window", L"Encryption", WS_OVERLAPPEDWINDOW, 100, 100, 1350, 450, NULL, NULL, hinstance, NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg = {};
+	while (GetMessage(&msg, hwnd, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
+}
+
+HRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wparam, LPARAM lparam) {
+	int columnInputString = 10;
+	int columnInputMove = columnInputString;
+	int columnOutputString = columnInputString + 630;
+	int columnButtonGo = columnInputString + 500;
+	int rowInputString = 10;
+	int rowInputMove = rowInputString + 220;
+	int rowOutputString = rowInputString;
+	int rowButtonGo = rowInputString + 220;
+
+	switch(uMsg) {
+
+	case WM_CREATE: {
+
+		inputString  = CreateWindow(L"EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_VSCROLL | WS_BORDER | ES_MULTILINE | ES_LEFT | ES_AUTOVSCROLL, columnInputString , rowInputString , 600, 200, hwnd, (HMENU)1, NULL, NULL);
+		outputString = CreateWindow(L"EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_VSCROLL | WS_BORDER | ES_MULTILINE | ES_LEFT | ES_AUTOVSCROLL, columnOutputString, rowOutputString, 600, 200, hwnd, (HMENU)2, NULL, NULL);
+
+		inputMove   = CreateWindow(L"EDIT"  , L"4"      , WS_VISIBLE | WS_BORDER | WS_CHILD                                , columnInputMove      , rowInputMove      , 90, 20, hwnd, (HMENU)3, NULL, NULL);
+		checkEncode = CreateWindow(L"BUTTON", L"Encode" , WS_VISIBLE | WS_BORDER | WS_CHILD | BS_AUTORADIOBUTTON | WS_GROUP, columnInputMove      , rowInputMove + 30 , 90, 20, hwnd, (HMENU)4, NULL, NULL);
+		checkDecode = CreateWindow(L"BUTTON", L"Decode" , WS_VISIBLE | WS_BORDER | WS_CHILD | BS_AUTORADIOBUTTON           , columnInputMove + 120, rowInputMove + 30 , 90, 20, hwnd, (HMENU)5, NULL, NULL);
+		checkCaesar = CreateWindow(L"BUTTON", L"Caesar" , WS_VISIBLE | WS_BORDER | WS_CHILD | BS_AUTORADIOBUTTON | WS_GROUP, columnInputMove      , rowInputMove + 60 , 90, 20, hwnd, (HMENU)6, NULL, NULL);
+		buttonGo    = CreateWindow(L"BUTTON", L"Execute", WS_VISIBLE | WS_BORDER | WS_CHILD                                , columnButtonGo       , rowButtonGo       , 90, 20, hwnd, (HMENU)7, NULL, NULL);
+		
+		checkFile   = CreateWindow(L"BUTTON", L"Export contents into file",   WS_VISIBLE | WS_BORDER | WS_CHILD | BS_AUTOCHECKBOX, columnInputMove, rowInputMove + 90, 270, 20, hwnd, (HMENU)8, NULL, NULL);
+	}
+				  
+	case WM_COMMAND:
+		switch (HIWORD(wparam)) {
+		case BN_CLICKED:
+			switch (LOWORD(wparam)) {
+			case 7: {
+
+
+			}
+
+			default:
+				break;
+			}
+
+		default:
+			break;
+		}
+
+	default:
+		break;
+
+	case WM_PAINT: {
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+		EndPaint(hwnd, &ps);
+		return 0;
+	}
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	}
+
+	return DefWindowProc(hwnd, uMsg, wparam, lparam);
 }
